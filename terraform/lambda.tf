fed824ea54afa2376c9a84a01ba6f74c35636c87
Martin Botha <your_email@example.com>
2025-06-17 15:37:19 +0200

    Fix Lambda zip paths to match GitHub Actions directory

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,8 +6,8 @@ resource "aws_lambda_function" "submit_log" {
   handler          = "handler.lambda_handler"
   runtime          = "python3.11"
   timeout          = 10
-  filename         = "${path.module}/../functions/submit_log/submit_log.zip"
-  source_code_hash = filebase64sha256("${path.module}/../functions/submit_log/submit_log.zip")
+  filename         = "${path.module}/submit_log.zip"
+  source_code_hash = filebase64sha256("${path.module}/submit_log.zip")
 }
 
 resource "aws_lambda_function" "get_logs" {
@@ -16,6 +16,7 @@ resource "aws_lambda_function" "get_logs" {
   handler          = "handler.lambda_handler"
   runtime          = "python3.11"
   timeout          = 10
-  filename         = "${path.module}/../functions/get_logs/get_logs.zip"
-  source_code_hash = filebase64sha256("${path.module}/../functions/get_logs/get_logs.zip")
+  filename         = "${path.module}/get_logs.zip"
+  source_code_hash = filebase64sha256("${path.module}/get_logs.zip")
 }
+
